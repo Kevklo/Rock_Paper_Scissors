c1532e296c09eb1d6c28cd8fe1cfb1537a964be7
Kunklo <126540566+Kunklo@users.noreply.github.com>
2023-09-05 00:30:13 -0300

    Added interactive buttons and on-screen result

--- script.js
+++ script.js
@@ -1,23 +1,6 @@
 function computerChoice() {
   //Every tool is asigned to a value, Rock = 0, Paper = 1, Scissor = 2//
-  num = Math.floor(Math.random() * 3);
-  if(num == 0){
-    return "rock";
-  } else if( num == 1){
-    return "paper";
-  }
-  return "scissors";
-}
-
-//Converts a tool to its numerical value//
-function toolToNumber(tool) {
-  if(tool == "rock"){
-    return 0;
-  } else if(tool == "paper"){
-    return 1;
-  } else if(tool == "scissors"){
-    return 2;
-  }
+  return Math.floor(Math.random() * 3);
 }
 
 //2, 1 and 0 represent win draw and lose respectively//
@@ -25,46 +8,48 @@ function toolToNumber(tool) {
 //the only other value that wins is (0,2), that explains the firts if//
 function playround(player, computer) {
   if(player - computer == 1 || (player == 0 && computer == 2)){
-    return(2);
+    const display_result = document.querySelector('.player_score');
+    let update_result = Number(`${display_result.innerText}`) + 1;
+    display_result.innerText = update_result;
   } else if(player == computer){
-    return(1);
+    console.log("draw");
   } else {
-    return(0);
+    const display_result = document.querySelector('.computer_score');
+    let update_result = Number(`${display_result.innerText}`) + 1;
+    display_result.innerText = update_result;
+  }
+
+  //--------------------------------Finish the match and show result-------------------------------//
+  if(Number(`${document.querySelector('.player_score').innerText}`) == 5){
+    while(document.querySelector('.result').firstChild){
+      document.querySelector('.result').removeChild(document.querySelector('.result').lastChild);
+    }
+    let win = document.createElement('p');
+    win.innerText = "You Win";
+    document.querySelector('.result').appendChild(win);
+  }
+  if(Number(`${document.querySelector('.computer_score').innerText}`) == 5){
+    while(document.querySelector('.result').firstChild){
+      document.querySelector('.result').removeChild(document.querySelector('.result').lastChild);
+    }
+    let win = document.createElement('p');
+    win.innerText = "You Lose";
+    document.querySelector('.result').appendChild(win);
   }
 }
 
-//Returns true is the Tool is Valid, false if not//
-function validTool(tool){
-  return (tool == "rock" || tool == "paper" || tool == "scissors");
-}
+const button_rock = document.querySelector('.rock');
+button_rock.addEventListener('click', () => {
+  let com_choice = computerChoice();
+  playround(0, com_choice);
+});
 
-function play() {
-  let wins = 0, loses = 0, result = 0;
-  let tool = "", com_tool = "";
-  //First to five wins//
-  while(loses < 5 && wins < 5){
-    tool = prompt("Select your tool").toLowerCase();
-    //if the tool is not an option, we make them choose another untill they choose a valid option//
-    while(!validTool(tool)){
-        console.error(`Not a valid tool, select 'rock', 'paper' or 'scissors'`);
-        tool = prompt("Select your tool").toLowerCase();
-    }
-    com_tool = computerChoice();
-    result = playround(toolToNumber(tool), toolToNumber(com_tool));
-    if(result == 2){
-      wins++;
-      console.log(`You win this round, ${tool} beats ${com_tool}`);
-    } else if (result == 1){
-      console.log(`Draw, both have chosen ${tool}`);
-    } else {
-      loses++;
-      console.log(`You lost this round, ${com_tool} beats ${tool}`);
-    }
-    console.log(`the score is ${wins} to ${loses}`);
-  }
-  if(wins == 5){
-    console.log(`You Won!!!`);
-  } else {
-    console.log(`You lost :(`)
-  }
-}
+const button_paper = document.querySelector('.paper');
+button_paper.addEventListener('click', () => {
+  playround(1, computerChoice());
+})
+
+const button_scissors = document.querySelector('.scissors');
+button_scissors.addEventListener('click', () => {
+  playround(2, computerChoice());
+})

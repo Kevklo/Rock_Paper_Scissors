d8afd8bb975c9113919a6c380fcafa619ddc6703
Kunklo <126540566+Kunklo@users.noreply.github.com>
2023-09-05 12:38:09 -0300

    Added retry button, improved logic

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function numberToTool(tool) {
 //2, 1 and 0 represent win draw and lose respectively//
 //when the player chooses paper or scissors and wins, player - computer == 1, because (1,0) is a win and (2,1) is a win//
 //the only other value that wins is (0,2), that explains the firts if//
-function playround(player, computer, player_score, computer_score) {
+function playround(player, computer) {
   if(player - computer == 1 || (player == 0 && computer == 2)){
     let res = 2;
     player_score++;
@@ -45,19 +45,17 @@ function finishMatch(won) {
   let retry = document.querySelector('.retry')
   let result = document.querySelector('.result');
   if(won == true){
-    while(result.firstChild != retry){
-      result.removeChild(result.firstChild);
-    }
+    document.querySelector('.score').innerText = "";
+    document.querySelector('.description').innerText = "";
     let win = document.createElement('p');
     win.innerText = "You Win";
-    result.insertBefore(win, retry);
+    result.insertBefore(win, result.firstChild);
   } else {
-    while(result.firstChild != retry){
-      result.removeChild(result.firstChild);
-    }
+    document.querySelector('.score').innerText = "";
+    document.querySelector('.description').innerText = "";
     let win = document.createElement('p');
     win.innerText = "You Lose";
-    result.insertBefore(win, retry);
+    result.insertBefore(win, result.firstChild);
   }
   retry.setAttribute('style', 'visibility: visible');
 }
@@ -67,45 +65,47 @@ let computer_score = 0;
 
 const button_rock = document.querySelector('.rock');
 button_rock.addEventListener('click', () => {
-  let res = playround(0, computerChoice(), player_score, computer_score);
-  if(res == 2){
-    player_score++;
-  } else if (res == 0){
-    computer_score++;
-  }
-  if(player_score == 5){
-    finishMatch(true);
-  } else if(computer_score == 5){
-    finishMatch(false);
+  if(player_score < 5 && computer_score < 5){
+    playround(0, computerChoice());
+    if(player_score == 5){
+      finishMatch(true);
+    } else if(computer_score == 5){
+      finishMatch(false);
+    }
   }
 });
 
 const button_paper = document.querySelector('.paper');
 button_paper.addEventListener('click', () => {
-  let res = playround(1, computerChoice(), player_score, computer_score);
-  if(res == 2){
-    player_score++;
-  } else if (res == 0){
-    computer_score++;
-  }
-  if(player_score == 5){
-    finishMatch(true);
-  } else if(computer_score == 5){
-    finishMatch(false);
+  if(player_score < 5 && computer_score < 5){
+    playround(1, computerChoice());
+    if(player_score == 5){
+      finishMatch(true);
+    } else if(computer_score == 5){
+      finishMatch(false);
+    }
   }
 })
 
 const button_scissors = document.querySelector('.scissors');
 button_scissors.addEventListener('click', () => {
-  let res = playround(2, computerChoice(), player_score, computer_score);
-  if(res == 2){
-    player_score++;
-  } else if (res == 0){
-    computer_score++;
+  if(player_score < 5 && computer_score < 5){
+    playround(2, computerChoice());
+    if(player_score == 5){
+      finishMatch(true);
+    } else if(computer_score == 5){
+      finishMatch(false);
+    }
   }
-  if(player_score == 5){
-    finishMatch(true);
-  } else if(computer_score == 5){
-    finishMatch(false);
-  }
+})
+
+const button_retry = document.querySelector('.retry');
+button_retry.addEventListener('click', () => {
+  player_score = 0;
+  computer_score = 0;
+  result = document.querySelector('.result');
+  result.removeChild(result.firstChild);
+  document.querySelector('.retry').setAttribute('style', 'visibility: hidden');
+  document.querySelector('.score').innerText = "0-0";
+  document.querySelector('.description').innerText = "";
 })
